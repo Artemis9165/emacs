73913f7257ac18464784e2eaf84f47d5f60f012d
Archemis <Artemis25265@gmail.com>
2026-07-27 14:05:25 -0400

    added some general settings and work on remaps

--- init.el
+++ init.el
@@ -1,5 +1,9 @@
 (require 'org)
 (require 'ob-tangle)
 (setq dotfiles-dir (expand-file-name "org/" user-emacs-directory))
-(mapc #'org-babel-load-file 
-      (directory-files-recursively dotfiles-dir "\\.org$"))
+(setq elpaca-config-file (expand-file-name "config/elpaca.org" dotfiles-dir))
+(when (file-exists-p elpaca-config-file)
+  (org-babel-load-file elpaca-config-file))
+(let ((all-org-files (directory-files-recursively dotfiles-dir "\\.org$")))
+  (mapc #'org-babel-load-file
+        (remove elpaca-config-file all-org-files)))
